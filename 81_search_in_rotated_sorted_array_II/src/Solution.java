b65d63c4678ec603138afe033757995c0bdac59a
zgs <zhengguanshi@gmail.com>
2015-08-22 15:24:55 +0800

    2015.8.22
    Finished 81, 173

--- 81_search_in_rotated_sorted_array_II/src/Solution.java
+++ 81_search_in_rotated_sorted_array_II/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 import tester.Tester;
 
 // Leetcode 81	
@@ -13,7 +15,8 @@ import tester.Tester;
 
 public class Solution {
     public boolean search(int[] nums, int target) {
-        return this.searchHelper(nums, 0, nums.length, target);
+//        return this.searchHelper(nums, 0, nums.length, target);
+    	return this.searchHelper(nums, 0, nums.length, target);
     }
     
     // WHERE: 0 <= start && start <= end && end <= nums.length;
@@ -29,28 +32,61 @@ public class Solution {
     		if(nums[mid] == target) {
     			return true;
     		}
-    		if(nums[start] < nums[mid]) {
-    			if(nums[start] <= target && target < nums[mid]) {
-    				return this.searchHelper(nums, start, mid, target);
+    		else if (nums[mid] < target) {
+    			if (nums[mid] < nums[end - 1]) {
+    				return 
+    				searchHelper(nums, start, mid, target)
+    				|| (Arrays.binarySearch(nums, mid + 1, end, target)) > -1 ;
+    			}
+    			else if (nums[mid] == nums[end - 1]) {
+    				return 
+    				searchHelper(nums, start, mid, target)
+    				|| linearSearch(nums, mid + 1, end, target);
     			}
     			else {
-    				return this.searchHelper(nums, mid + 1, end, target);
-    			}
-    		}
-    		else if(nums[start] > nums[mid]) {
-    			if(nums[start] < target || target < nums[mid]) {
-    				return this.searchHelper(nums, start, mid, target);
-    			}
-    			else {
-    				return this.searchHelper(nums, mid + 1, end, target);
+    				//@assert nums[mid] > nums[end - 1];
+    				return 
+    				Arrays.binarySearch(nums, start, mid, target) > -1
+    				|| searchHelper(nums, mid + 1, end, target);
     			}
     		}
     		else {
-    			//@assert nums[start] == nums[mid];
-    			
+    			//@assert nums[mid] > target;
+    			if (nums[start] < nums[mid]) {
+    				return 
+    				Arrays.binarySearch(nums, start, mid, target) > -1
+    				|| searchHelper(nums, mid + 1, end, target);
+    			}
+    			else if (nums[start] == nums[mid]) {
+    				return 
+    				linearSearch(nums, start, mid, target)
+    				|| searchHelper(nums, mid + 1, end, target);
+    			}
+    			else {
+    				//@assert nums[start] > nums[mid];
+    				return 
+    				searchHelper(nums, start, mid, target) 
+    				|| Arrays.binarySearch(nums, mid + 1, end, target) > -1;
+    			}
     		}
     	}
     }
+    
+    // GIVEN: a sub-array nums[start, end) and a target value
+    // WHERE: 0 <= start && start <= end && end <= nums.length;
+    // RETURNS: true iff the given sub-array contains an element equal to 
+    // target
+    private boolean linearSearch(int[] nums, int start, int end, int target) {
+    	for (int i = start; i < end; i++) {
+    		if (nums[i] == target) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+   
+    
+
 }
 
 class SolutionExamples {
@@ -77,11 +113,12 @@ class SolutionExamples {
 	boolean testSearch(Tester t) {
 		return
 		t.checkExpect(this.checkSearch(new int[] {1, 3, 1}, 3), true) &&
-		t.checkExpect(this.checkSearch(new int[] {1, 3, 1}, 2), false) &&
+		t.checkExpect(this.checkSearch(new int[] {1, 3, 1}, 2), true) &&
 		t.checkExpect(this.checkSearch(new int[] {1, 0}, 1), true) &&
-		t.checkExpect(this.checkSearch(new int[] {4, 5, 6, 7, 0, 1, 2}, 3), false) &&
+		t.checkExpect(this.checkSearch(new int[] {4, 5, 6, 7, 0, 1, 2}, 3), true) &&
 		t.checkExpect(this.checkSearch(new int[] {4, 5, 6, 7, 0, 1, 2}, 5), true) &&
 		t.checkExpect(this.checkSearch(new int[] {0, 1, 2, 4, 5, 6, 7}, 0), true) &&
-		t.checkExpect(this.checkSearch(new int[] {4, 5, 6, 7, 0, 1, 2}, 2), true);
+		t.checkExpect(this.checkSearch(new int[] {4, 5, 6, 7, 0, 1, 2}, 2), true) &&
+		t.checkExpect(this.checkSearch(new int[]{1,3,1,1}, 3), true);
 	}
 }
